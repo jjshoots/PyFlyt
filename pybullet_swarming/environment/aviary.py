c9f45f20cd20b4d10eabc353eef84f687747b18d
Jet <taijunjet@hotmail.com>
2021-12-01 01:06:29 +0000

    environment file was missing because of gitignore

---
+++ pybullet_swarming/environment/aviary.py
@@ -0,0 +1,94 @@
+import time
+import numpy as np
+import multiprocessing as mp
+
+import pybullet as p
+import pybullet_data
+from pybullet_utils import bullet_client
+
+from pybullet_swarming.environment.drone import *
+
+class Aviary(bullet_client.BulletClient):
+    def __init__(self, start_pos: np.ndarray, start_orn: np.ndarray, render=False):
+        super().__init__(p.GUI if render else p.DIRECT)
+
+        # default physics looprate is 240 Hz
+        self.period = 1. / 240.
+        self.now = time.time()
+
+        self.start_pos = start_pos
+        self.start_orn = start_orn
+
+        self.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self.render = render
+        self.reset()
+
+
+    def reset(self):
+        self.resetSimulation()
+        self.setGravity(0, 0, -9.81)
+        self.step_count = 0
+
+        """ CONSTRUCT THE WORLD """
+        self.planeId = self.loadURDF(
+            "plane.urdf",
+            useFixedBase=True,
+            globalScaling=np.random.rand() * 20. + 1.
+        )
+
+        # spawn drones
+        self.drones = []
+        for start_pos, start_orn in zip(self.start_pos, self.start_orn):
+            self.drones.append(Drone(self, start_pos=start_pos, start_orn=start_orn))
+
+
+    @property
+    def num_drones(self):
+        return len(self.drones)
+
+
+    @property
+    def states(self):
+        """
+        returns a list of states for each drone in the aviary
+        """
+        states = []
+        for drone in self.drones:
+            states.append(drone.state)
+
+        states = np.stack(states, axis=0)
+
+        return states
+
+
+    def set_mode(self, flight_mode):
+        """
+        sets the flight mode for each drone
+        """
+        for drone in self.drones:
+            drone.set_mode(flight_mode)
+
+
+    def set_setpoints(self, setpoints):
+        """
+        commands each drone to go to a setpoint as specified in a list
+        """
+        for i, drone in enumerate(self.drones):
+            drone.setpoint = setpoints[i]
+
+
+    def step(self):
+        """
+        Steps the environment
+        """
+        if self.render:
+            elapsed = time.time() - self.now
+            time.sleep(max(self.period - elapsed, 0.))
+            self.now = time.time()
+
+            # print(f'RTF: {self.period / (elapsed + 1e-6)}')
+
+        for drone in self.drones: drone.update()
+
+        self.stepSimulation()
+        self.step_count += 1
---
+++ pybullet_swarming/environment/simulator.py
@@ -0,0 +1,67 @@
+import copy
+import numpy as np
+
+from pybullet_swarming.environment.environment import *
+from pybullet_swarming.utility.shebangs import  *
+from pybullet_swarming.flier.swarm_controller import *
+
+class Simulator():
+    """
+    Class wrapper around `environment` to be concise with the swarm controller
+    Control is done using linear velocity setpoints and yawrate:
+        vx, vy, vz, vr
+    States is full linear position and yaw
+        x, y, z, r
+    """
+    def __init__(self, start_pos, start_orn):
+
+        # instantiate the digital twin
+        self.env = Aviary(start_pos=start_pos, start_orn=start_orn, render=True)
+        self.env.set_mode(6)
+
+        # keep track of runtime
+        self.steps = 0
+
+
+    def set_setpoints(self, setpoints: np.ndarray):
+        """
+        setpoints is a num_drones x 4 array, where the 4 corresponds to vx, vy, vz, vr
+        """
+        # the setpoints in the digital twin has the last two dims flipped
+        temp = copy.deepcopy(setpoints[:, -2])
+        setpoints[:, -2] = copy.deepcopy(setpoints[:, -1])
+        setpoints[:, -1] = temp
+        self.env.set_setpoints(setpoints)
+
+
+    def step(self):
+        self.steps += 1
+        self.env.step()
+
+
+    def get_states(self):
+        states = np.zeros((self.num_drones, 4))
+        states[:, :-1] = copy.deepcopy(self.env.states[:, -1, :])
+        states[:, -1] = copy.deepcopy(self.env.states[:, 1, -1])
+
+        return states
+
+
+    def sleep(self, seconds: float):
+        for _ in range(int(seconds / self.env.period)):
+            self.step()
+
+
+    @property
+    def states(self):
+        return self.get_states()
+
+
+    @property
+    def num_drones(self):
+        return self.env.num_drones
+
+
+    @property
+    def elapsed_time(self):
+        return self.env.period * self.steps
