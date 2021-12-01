e4086fbc8543897755c33b5d3cfe50c1984ff8ac
Jet <taijunjet@hotmail.com>
2021-12-01 14:09:43 +0000

    added missing dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='pybullet_swarming',
     version='1.0.0',
     install_requires=[
+        'wandb',
         'numpy',
         'matplotlib',
         'pybullet',
