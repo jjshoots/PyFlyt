18cd5df1d5e02e1e1fc0cf7c83d01ccc5e05f601
Jet <taijunjet@hotmail.com>
2022-05-28 15:14:29 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 from setuptools import setup
 
-with open("readme.md") as fh:
-    long_description = ""
-    header_count = 0
-    for line in fh:
-        if line.startswith("##"):
-            header_count += 1
-        else:
-            long_description += line
 
 setup(
     name="PyFlyt",
-    version="0.0.1",
+    version="0.0.3",
     author="jjshoots",
     author_email="taijunjet@hotmail.com",
     description="Freestyle Quadcopter Flight in Pybullet with Gym and (soon) PettingZoo APIs",
     url="https://github.com/jjshoots/PyFlyt",
-    long_description=long_description,
+    long_description="# [Docs](https://github.com/jjshoots/PyFlyt/blob/master/readme.md)",
     long_description_content_type="text/markdown",
     keywords=["Reinforcement Learning", "UAVs", "drones", "Quadcopter", "AI", "Gym", "PettingZoo"],
     python_requires=">=3.8, <3.11",
