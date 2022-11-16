408f44098c0cf95c0afa893ca0dba6d4d81a6935
Jet <taijunjet@hotmail.com>
2022-11-16 23:40:07 +0000

    version buymp

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="PyFlyt",
-    version="0.2.2",
+    version="0.2.3",
     author="jjshoots",
     author_email="taijunjet@hotmail.com",
     description="Freestyle Quadcopter Flight in Pybullet with Gym and (soon) PettingZoo APIs",
