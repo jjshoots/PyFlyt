33e3ab73ab79e9765f621336bce251167be50ec0
jjshoots <taijunjet@hotmail.com>
2022-11-30 22:06:48 +0000

    version bump, finish aviary tutorial

---
+++ examples/aviary/07_custom_uav_model.py
@@ -0,0 +1,17 @@
+"""Spawn a single drone on x=0, y=0, z=1, with 0 rpy."""
+import numpy as np
+from PyFlyt import Aviary
+
+# the starting position and orientations
+start_pos = np.array([[0.0, 0.0, 1.0]])
+start_orn = np.array([[0.0, 0.0, 0.0]])
+
+# environment setup
+env = Aviary(start_pos=start_pos, start_orn=start_orn, model_dir="./", drone_model="new_uav", render=True)
+
+# set to position control
+env.set_mode(7)
+
+# simulate for 1000 steps (1000/120 ~= 8 seconds)
+for i in range(1000):
+    env.step()
