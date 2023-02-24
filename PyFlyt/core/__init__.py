4e0f7e194bc6ef9b7ad33e4bd068bbb5da089ded
jjshoots <taijunjet@hotmail.com>
2023-02-24 18:38:23 +0000

    remove unnecessary imports

--- PyFlyt/core/__init__.py
+++ PyFlyt/core/__init__.py
@@ -2,7 +2,4 @@ from .abstractions.base_controller import CtrlClass
 from .abstractions.base_drone import DroneClass
 from .abstractions.pid import PID
 from .aviary import Aviary
-from .drones.fixedwing import FixedWing
-from .drones.quadplane import Quadplane
-from .drones.quadx import QuadX
 from .load_objs import loadOBJ, obj_collision, obj_visual
