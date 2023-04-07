b5c9354e1a1a74be42e72ee2e36020b91aba3166
jjshoots <taijunjet@hotmail.com>
2023-04-07 22:54:27 +0100

    update

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -23,5 +23,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinxawesome_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
