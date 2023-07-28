47c20b23f5827389dd3f353762b435356b39940e
jjshoots <taijunjet@hotmail.com>
2023-07-28 13:21:07 +0100

    fix checks

--- PyFlyt/utils/checks.py
+++ PyFlyt/utils/checks.py
@@ -1,22 +1,24 @@
 """Common checks."""
 import warnings
 
+import pybullet as p
 from gymnasium.utils import colorize
 
 
 def check_numpy():
     """Checks that numpy is installed."""
-    warnings.warn(
-        colorize(
-            (
-                "PyBullet is not installed properly with Numpy functionality! This will "
-                "result in a significant performance hit when vision is used.\n\n"
-                "You can fix this by installing Numpy again and rebuilding PyBullet:\n"
-                "\tpip3 uninstall pybullet -y\n"
-                "\tpip3 install numpy\n"
-                "\tpip3 install pybullet --no-cache-dir"
+    if not p.isNumpyEnabled():
+        warnings.warn(
+            colorize(
+                (
+                    "PyBullet is not installed properly with Numpy functionality! This will "
+                    "result in a significant performance hit when vision is used.\n\n"
+                    "You can fix this by installing Numpy again and rebuilding PyBullet:\n"
+                    "\tpip3 uninstall pybullet -y\n"
+                    "\tpip3 install numpy\n"
+                    "\tpip3 install pybullet --no-cache-dir"
+                ),
+                color="yellow",
             ),
-            color="yellow",
-        ),
-        category=RuntimeWarning,
-    )
+            category=RuntimeWarning,
+        )
