d0696f4230cda130c1b50631aaa815872777937d
Jet <taijunjet@hotmail.com>
2024-07-01 01:39:16 +0900

    modify reward

--- PyFlyt/gym_envs/quadx_envs/quadx_pole_waypoints_env.py
+++ PyFlyt/gym_envs/quadx_envs/quadx_pole_waypoints_env.py
@@ -230,7 +230,7 @@ class QuadXPoleWaypointsEnv(QuadXBaseEnv):
         if not self.sparse_reward:
             self.reward += max(3.0 * self.waypoints.progress_to_target(), 0.0)
             self.reward += 0.1 / self.distance_to_immediate
-            self.reward += 1.0 - self.pole.leaningness
+            self.reward += 0.5 * (1.0 - self.pole.leaningness)
 
         # target reached
         if self.waypoints.target_reached():
