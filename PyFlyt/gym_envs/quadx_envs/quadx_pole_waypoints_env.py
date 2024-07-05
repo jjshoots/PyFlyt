7532cc1cb218c4fc3a30e734e9818340e8e5b041
Jet <taijunjet@hotmail.com>
2024-07-05 13:48:40 +0900

    revert reward

--- PyFlyt/gym_envs/quadx_envs/quadx_pole_waypoints_env.py
+++ PyFlyt/gym_envs/quadx_envs/quadx_pole_waypoints_env.py
@@ -230,12 +230,12 @@ class QuadXPoleWaypointsEnv(QuadXBaseEnv):
         # bonus reward if we are not sparse
         if not self.sparse_reward:
             self.reward += max(10.0 * self.waypoints.progress_to_next_target, 0.0)
-            self.reward += 0.4 / self.waypoints.distance_to_next_target
+            self.reward += 0.5 / self.waypoints.distance_to_next_target
             self.reward += (0.5 - self.pole.leaningness)
 
         # target reached
         if self.waypoints.target_reached:
-            self.reward = 500.0
+            self.reward = 300.0
 
             # advance the targets
             self.waypoints.advance_targets()
