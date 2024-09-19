46ed3ed9052cc4d2738eaf482a8b9aa460129ac8
Jet <taijunjet@hotmail.com>
2024-09-19 18:26:58 +0900

    fix bug

--- PyFlyt/pz_envs/fixedwing_envs/ma_fixedwing_dogfight_env.py
+++ PyFlyt/pz_envs/fixedwing_envs/ma_fixedwing_dogfight_env.py
@@ -582,7 +582,7 @@ class MAFixedwingDogfightEnv(MAFixedwingBaseEnv):
             )
 
             # reward for being too close to anyone, minus one to ignore self
-            too_close = delta_distance < self.too_close_distance
+            too_close = self.current_distances < self.too_close_distance
             engagement_rewards -= (np.sum(too_close, axis=-1) - 1) * 20.0
 
         # reward for hits, penalty for being hit
